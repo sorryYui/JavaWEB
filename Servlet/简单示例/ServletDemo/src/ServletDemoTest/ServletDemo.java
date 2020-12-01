5df8dc31c1ddc97d454381b3d12687a0dd091178
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-01 21:44:47 +0800

    Add files via upload

---
+++ Servlet/简单示例/ServletDemo/src/ServletDemoTest/ServletDemo.java
@@ -0,0 +1,22 @@
+package ServletDemoTest;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+public class ServletDemo extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request,HttpServletResponse response)
+        throws IOException,ServletException
+    {
+        doPost(request,response);
+    }
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)
+            throws IOException,ServletException
+    {
+        response.setContentType("test/html;charset = utf-8");
+        PrintWriter out = response.getWriter();
+        out.write("HelloWrold!\n");
+    }
+}
