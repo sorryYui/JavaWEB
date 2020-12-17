f7424b951f30f6cef662f44219f6b6c7cf493a20
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-17 16:07:00 +0800

    Create Test.java

---
+++ JSP/标签/自定义标签/简单标签(SimpleTag)/示例/Test.java
@@ -0,0 +1,11 @@
+<%@ page language = "java" pageEncoding = "UTF-8"%>
+<%@taglib uri = "/ServletDemoTest" prefix = "ServletDemoTest"%>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <ServletDemoTest:TagTest> HHHHH </ServletDemoTest:TagTest>
+    </body>
+</html>
