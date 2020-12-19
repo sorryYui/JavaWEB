a32d94078def1a88fd79480d8d40ede024923778
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-19 19:46:00 +0800

    Create personBean.java

---
+++ JSP/标签/JSTL核心标签/set标签/示例/personBean.java
@@ -0,0 +1,35 @@
+package ServletDemoTest;
+public class PersonBean
+{
+    private String age;
+    private String name;
+    private String country;
+    public PersonBean()
+    {
+
+    }
+    public void setAge(String age)
+    {
+        this.age = age;
+    }
+    public String getAge()
+    {
+        return age;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+    public String getCountry()
+    {
+        return country;
+    }
+}
