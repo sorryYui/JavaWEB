e2b3d23f0ed8c25f86023bdebf4b944777eb0f92
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-21 13:43:31 +0800

    Create UserBean.java

---
+++ JSP/EL表达式/执行运算/示例/UserBean.java
@@ -0,0 +1,26 @@
+package ServletDemoTest;
+public class UserBean
+{
+    private String name;
+    private String sex;
+    public UserBean()
+    {
+
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getSex()
+    {
+        return sex;
+    }
+}
