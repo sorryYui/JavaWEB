ce50e829b6e76c93334849cb6d3680db68f3825b
sorryYui <74042501+sorryYui@users.noreply.github.com>
2020-12-21 10:27:50 +0800

    Create personBean.java

---
+++ JSP/EL表达式/数据获取/示例/personBean.java
@@ -0,0 +1,35 @@
+package ServletDemoTest;
+public class PersonBean
+{
+    private String name;
+    private String address;
+    private String age;
+    public PersonBean()
+    {
+
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+    public void setAge(String age)
+    {
+        this.age = age;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
+    public String getAge()
+    {
+        return age;
+    }
+}
