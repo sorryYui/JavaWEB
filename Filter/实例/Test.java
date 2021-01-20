4eab8a90db9faf58782d5825e0e3bff347b29965
sorryYui <74042501+sorryYui@users.noreply.github.com>
2021-01-20 12:39:20 +0800

    Create Test.java

---
+++ Filter/实例/Test.java
@@ -0,0 +1,19 @@
+package ServletDemoTest;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+public class Test extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request,HttpServletResponse response)
+        throws IOException,ServletException
+    {
+
+    }
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)
+        throws IOException,ServletException
+    {
+
+    }
+}
